f47bc5a1e30fe871e9125055c3c3f1edb5b8f1dd
harun0x <149308629+harun0x@users.noreply.github.com>
2023-12-29 14:23:14 -0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,128 @@
+confirm("Are you sure you want to delete?")
+$(document).ready(function(){
+
+    $(".mudaTela").click(function(){
+        mudaTela( $(this), $(this).attr("nova"), $(this).attr("animacao"), $(this).attr("tempoAnimacao") );
+    });
+
+    $("a.opcoes").click(function(e){
+        e.preventDefault();
+        $("div.opcoes").slideToggle(500);
+    });
+
+    $(".calendario .marcado").click(function(){
+        mostraMsgMes($(this).attr("value"));
+    });
+
+    const mudaTela = ( atual, nova = null, animacao = "fade", tempoAnimacao = 900 ) => {
+
+        // define a nova tela
+        if(!nova){
+            nova = parseInt(atual.parent().attr("id").split("tela")[1])+1;
+        }
+
+        if(animacao == "fade"){
+            $("#tela"+(nova-1)).fadeOut(tempoAnimacao);
+            setTimeout(() => {
+                $("#tela"+nova).fadeIn(tempoAnimacao)
+            }, tempoAnimacao);
+        }else{
+            $("#tela"+(nova-1)).hide(tempoAnimacao);
+            $("#tela"+nova).show(tempoAnimacao);
+        }
+
+        if($("#tela"+nova).hasClass("temporizado")){
+            $("#tela"+nova+" div").hide();
+            telaTemporizada(nova, 0);
+        }
+
+        verificaFundo(nova);
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        if(nova == 5){
+            var audio = new Audio('assets/musica.mp3');
+            audio.volume = 0.1;
+            audio.play();
+        }
+        
+    }
+
+    const telaTemporizada = ( nTela, contador ) =>{
+
+        const tela = $("#tela"+nTela+" div:eq("+contador+")");
+        const temporizador = 500;
+        const temporizadorPrimeiraTela = (contador==0?$("#tela"+nTela).attr("tempo"):temporizador);
+
+        setTimeout(() => {
+            tela.fadeIn(temporizador);
+
+            setTimeout(() => {
+                tela.fadeOut(temporizador);
+                if(tela.attr("final") == "true"){
+                    mudaTela(null, nTela+1, "fade", 900);
+                    verificaFundo(nTela+1);
+                }else{
+                    telaTemporizada(nTela, contador+1);
+                }
+
+            }, tela.attr("tempo") );
+
+        }, temporizadorPrimeiraTela);
+        
+    }
+
+    const verificaFundo = (nTela) =>{
+
+        const fundo = $("#tela"+nTela).attr("fundo");
+        const tempo = $("#tela"+nTela).attr("tempo");
+
+        if(fundo){
+            $("body").attr("class", fundo);            
+        }
+        
+    }
+
+    const mostraMsgMes = (texto) =>{
+
+        let titulo;
+        let mensagem;
+
+        switch(texto){
+            case "9/8": titulo = "09 de Agosto de 2021"; mensagem = "<p>O dia em que nós conhecemos, esse dia foi tao especial.</p>";break;
+            case "16/9": titulo = "16 de Outubro de 2022"; mensagem = "<p>Aqui foi quando voce me pediu em namoro, eu chorei de tanta felicidade.</p>";break;
+            case "final": titulo = "31 de Dezembro de 2023"; mensagem = "<section class='text-center mt-5 mb-5'><p><strong>O dia em que ela disse<br><span class='letra2 letra-vermelha'>SIM</span></strong></p></section>";break;
+        }
+
+        mostraPopUp(true, titulo, mensagem);
+        telaFinal = (texto=="final"?true:false);
+    }
+
+    
+
+});
+
+let telaFinal = false;
+
+const mostraPopUp = (mostrar, titulo = "Título de testes", mensagem = "Mensagem de teste...") =>{
+
+    if(mostrar){
+        $("html, body").animate({ scrollTop: $(".pop-up")[0].offsetTop }, "smooth");
+        $(".pop-up").fadeIn(500);
+        $(".pop-up h1").html(titulo);
+        $(".pop-up div").html(mensagem);
+        $(".container").css("opacity", "0.5");
+    }else{
+        $(".pop-up").fadeOut(500);
+        $(".container").css("opacity", "1");
+
+        if(telaFinal){
+            $("#tela19").fadeOut(4000);
+            setTimeout(() => {
+                $("#tela20").fadeIn(6500);
+                $("body").attr("class", "fundo6");    
+                $("html, body").animate({ scrollTop: 0 }, "slow");
+            }, 4000);
+        }
+
+    }
+
+}
